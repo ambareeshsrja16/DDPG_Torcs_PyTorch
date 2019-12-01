96c4e5f91d06c4a798cff1368316010a4d4fb12a
Ambareesh S N Jayakumari <ambareeshsrja16@gmail.com>
2019-12-01 15:18:52 -0800

    Update gym_torcs.py
    Fixed bugs in Observation namedtuple

--- gym_torcs.py
+++ gym_torcs.py
@@ -264,7 +264,10 @@ class TorcsEnv:
                                wheelSpinVel=np.array(raw_obs['wheelSpinVel'], dtype=np.float32))
         else:
             names = ['focus',
-                     'speedX', 'speedY', 'speedZ', 'angle',
+                     'speedX',
+                     'speedY', 
+                     'speedZ', 
+                     'angle',
                      'opponents',
                      'rpm',
                      'track',
@@ -274,7 +277,7 @@ class TorcsEnv:
             Observation = col.namedtuple('Observaion', names)
 
             # Get RGB from observation
-            image_rgb = self.obs_vision_to_image_rgb(raw_obs[names[10]])
+            image_rgb = self.obs_vision_to_image_rgb(raw_obs[names[8]])
 
             return Observation(focus=np.array(raw_obs['focus'], dtype=np.float32)/200.,
                                speedX=np.array(raw_obs['speedX'], dtype=np.float32)/self.default_speed,
